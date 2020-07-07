17678b62055e8ce5d3fe7f6e85214894beb21c90
Alexander East <alexander.w.east@gmail.com>
2020-07-07 12:58:40 -0400

    Added inital commit of weights and PK

---
+++ Weights and PK.R
@@ -0,0 +1,52 @@
+# 
+# Weighting Formula and PK Function
+#
+# LE Model by AE & KP 7/7/2020
+# 
+
+rm(list = ls())
+
+
+WM <- function(x,w){
+  
+  ans <-sum(x*w)/sum(w)
+  return(ans)
+}
+
+# x = mean
+# w = weight
+
+
+
+WSD <- function(x,wm,w){
+  
+M           <- as.numeric(length(w[w > 0]))
+numerator   <- sum(w*(x- wm)^2)
+denominator <- (M - 1)/M * (sum(wm))
+ans         <- numerator / denominator
+
+return(ans)                            
+}
+
+# x = mean
+# wm = weighted mean
+# M = number of non zero weights
+
+
+Simple.PK <- function(DP,kP,Vd){
+  
+  CP <- DP/(kP * Vd)
+  
+  return(CP)
+}
+
+# DP = dose (ng/kg bw/day)
+# CP = serum conc. ng/mL
+# Vd = volume distribution (mL/kg bw)
+# kP = elimination rate (day -1)
+
+
+# Test
+WM(1:5,2:6)
+WSD(1:5,0:4,2:6)
+Simple.PK(5,.0008,170)
