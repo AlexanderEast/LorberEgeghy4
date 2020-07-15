765d86a015b08ef5ee88489ea3a6e9ae5a04f81d
Alexander East <alexander.w.east@gmail.com>
2020-07-15 11:34:55 -0400

    Added NHANES Blood

--- NHANES Blood.R
+++ NHANES Blood.R
@@ -10,15 +10,15 @@ blood <- read_excel('input/NHANES PFAS.xlsx', sheet = 'PFAS_All', guess_max = 17
 # 2. Tidy
 
 blood <- blood %>% select(Cycle, RIAGENDR, RIDAGEYR, SSNPFOA, SSBPFOA, SSNPFOS, SSMPFOS) %>%
-                   filter(RIDAGEYR >= 12 & Cycle >= 20132014) 
+  filter(RIDAGEYR >= 12 & Cycle >= 20132014) 
 
 blood<- data.frame(blood)
 blood[is.na(blood)] <- 0
 blood <- blood %>% mutate(PFOA = (SSNPFOA + SSBPFOA)*1000,
                           PFOS = (SSNPFOS + SSMPFOS)*1000)%>%
-                   select(-SSBPFOA,-SSNPFOA,-SSMPFOS,-SSNPFOS) %>%
-                   mutate( RIAGENDR = case_when( (RIAGENDR == "1") ~ "Male",
-                                                 (RIAGENDR == "2") ~ "Female"))
+  select(-SSBPFOA,-SSNPFOA,-SSMPFOS,-SSNPFOS) %>%
+  mutate( RIAGENDR = case_when( (RIAGENDR == "1") ~ "Male",
+                                (RIAGENDR == "2") ~ "Female"))
 names(blood)[1:3]<- c("Year","Gender","Age")
 # units are ug/ L per Labels sheet in NHANES input file, hence*1000 above
 # to switch to ng/L. 
@@ -30,22 +30,22 @@ names(blood)[1:3]<- c("Year","Gender","Age")
 # values reported in paper as ng/mL (2011 PFOA paper)
 
 blood <- blood %>% mutate(PFOA = PFOA/1000,
-                 PFOS = PFOS/1000)
+                          PFOS = PFOS/1000)
 
 blood <- split(blood,list(blood$Gender,blood$Year))
 
 
 
 get.blood.summary <- function(m){
-
-PFOA <- quantile(m$PFOA,c(0.1,0.5,0.95,0.99))
-PFOS <- quantile(m$PFOS,c(0.1,0.5,0.95,0.99))
-result <- rbind(PFOA,PFOS)
-result <- cbind(rownames(result),rep(m$Year[1],2),"ng/mL",result)
-colnames(result)<- c("Chemical","Year",
-                     "Units","10%","Median","95%","99%")
-
-return(result)
+  
+  PFOA <- quantile(m$PFOA,c(0.1,0.5,0.95,0.99))
+  PFOS <- quantile(m$PFOS,c(0.1,0.5,0.95,0.99))
+  result <- rbind(PFOA,PFOS)
+  result <- cbind(rownames(result),rep(m$Year[1],2),"ng/mL",result)
+  colnames(result)<- c("Chemical","Year",
+                       "Units","10%","Median","95%","99%")
+  
+  return(result)
 }
 
 
